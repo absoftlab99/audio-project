05668e02bd450f3d3a002bb3d8e8f918a06cd23a
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-08-24 08:15:25 +0600

    audio picker updated

--- js/music.js
+++ js/music.js
@@ -1,6 +1,10 @@
 document.getElementById('btn1').addEventListener('click', function(){
     const disk = document.getElementById('img');
     disk.classList.add('img')
+    disk.src = './images/21FebruaryDisk.svg'
+
+    const elips = document.getElementById('elips');
+    elips.classList.add('img');
 
     const song = document.getElementById('song');
     song.src = './audios/amarVaierRokteRangano.mp3';
